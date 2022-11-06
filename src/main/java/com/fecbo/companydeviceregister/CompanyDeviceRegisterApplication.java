7edfdb0b9100d93891f368571d9b46432ac59a5e
Botond Feczkó <feczko.botond@hallgato.ppke.hu>
2022-11-06 13:47:34 +0100

    master: init

---
+++ src/main/java/com/fecbo/companydeviceregister/CompanyDeviceRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.fecbo.companydeviceregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyDeviceRegisterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompanyDeviceRegisterApplication.class, args);
+    }
+
+}
