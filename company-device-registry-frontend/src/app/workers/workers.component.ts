b25e5c69daf11d9d2fc466ba1ec6a90cd05b0b40
Botond Feczkó <feczko.botond@hallgato.ppke.hu>
2022-11-21 22:44:14 +0100

    master: adding worker component

---
+++ company-device-registry-frontend/src/app/workers/workers.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import {WorkerService} from '../services/worker.service';
+
+@Component({
+  selector: 'app-workers',
+  templateUrl: './workers.component.html',
+  styleUrls: ['./workers.component.css']
+})
+export class WorkersComponent implements OnInit {
+
+  workers : Worker[] = [];
+
+  constructor(private workerService: WorkerService) { }
+
+  ngOnInit(): void {
+    this.initWorkers();
+  }
+
+  private initWorkers() {
+    this.workerService.getWorkers().subscribe(workers => this.workers = workers);
+  }
+}
